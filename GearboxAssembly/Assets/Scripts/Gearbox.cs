ae58e749fa378fbe2f8b188657160ed71710d148
Nick Swanson <ncs72mc@gmail.com>
2025-09-30 11:48:46 -0400

    feat: :construction: Started writing classes for assembly logic and how to store the steps

---
+++ GearboxAssembly/Assets/Scripts/Gearbox.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+
+
+
+// class manages the the gearbox assembly process, the part interactions, manages the parts
+public class Gearbox : MonoBehaviour {
+
+
+
+
+
+
+    private GameObject[] parts;
+
+
+    private void FixedUpdate() {
+
+    }
+
+
+
+    // event listeners
+    private void OnTriggerEnter(Collider other) {}
+
+
+
+    // Private Methods
+    private void CollectParts() {}
+
+
+    private void AssembleGearbox() {}
+
+
+
+
+    // collect all parts of the gearbox
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ GearboxAssembly/Assets/Scripts/TrainingManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+
+// this class handles the training state
+public class TrainingManager : MonoBehaviour {
+
+
+    // Private Fields
+    private int currentStep;
+    private int totalSteps;
+    private bool isTrainingActive;
+
+    private Gearbox gearbox;
+    private Workstation workstation;
+
+
+
+
+
+
+
+    // Public Methods
+    public void StartTraining() {}
+
+    public void nextStep() {}
+    public void previousStep() {}
+
+    public void EndTraining() {}
+
+    public void ShowHint() {}
+
+
+    // Private Methods
+    private void UpdateUI() {}
+
+
+    private void HighlightCurrentPart() {}
+
+    private void ShowGhostPart() {}
+
+
+    private void LogProgress() {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
